98b75e1b7eac1965f8163790a6463cae9a24ee59
Chesser2017 <chesser201710@gmail.com>
2019-07-28 15:26:11 +0600

    project is now usable, but i'd like to improve the ui a bit later

--- main.js
+++ main.js
@@ -1,58 +1,54 @@
-function setTitle(title, color){
-    title.style.color = color;
-    title.style.textAlign = 'center';
-    title.style.fontSize = '30px';
-}
-
 function setUpperBox(element, background, borderColor){
-    element.style.borderRadius = '16px';
-    element.style.border = `4px solid ${borderColor}`;
-    element.style.backgroundColor = background;
-    element.style.width = '256px';
-    element.style.height = '166px';
-    element.style.margin = 'auto';
+    if(background){
+        element.style.backgroundColor = background;
+    }
+    if(borderColor){
+        element.style.border = `4px solid ${borderColor}`;
+    }
 }
-
-function createHeroCircle(circle, color){
-    circle.style.width = '60px';
-    circle.style.height = '60px';
-    circle.style.margin = 'auto';
-    circle.style.backgroundColor = color;
-    circle.style.borderRadius = '100%';
-}
-
-
 
 function createHero(){
     //Values from user
-    let heroName = document.getElementById('hero-name').value;
-    let firstAbility = document.getElementById('ability-one').value;
+    let name = document.getElementById('name').value;
+    let abilityOne = document.getElementById('ability-one').value;
     let descriptionOne = document.getElementById('description-one').value;
-    let secondAbility = document.getElementById('ability-two').value;
+    let abilityTwo = document.getElementById('ability-two').value;
     let descriptionTwo = document.getElementById('description-two').value;
-    let heroColor = document.getElementById('hero-color').value;
+    let color = document.getElementById('color').value;
     let background = document.getElementById('background-color').value;
 
     //Elements to change
-    let newHero = document.getElementById('new-hero');
-    let newHeroCircle = document.getElementById('new-hero-circle');
-    let newAbilities = document.getElementById('new-abilities');
-    let newheroName = document.getElementById('new-hero-name');
-    let newfirstAbility = document.getElementById('new-ability-one');
-    let newdescriptionOne = document.getElementById('new-description-one');
-    let newsecondAbility = document.getElementById('new-ability-two');
-    let newdescriptionTwo = document.getElementById('new-description-two');
+    let heroBox = document.getElementById('hero-box');
+    let abilityBox = document.getElementById('ability-box');
+    let abilities = document.getElementsByClassName('ability');
+    let currentName = document.getElementById('current-name');
+    let currentAbilityOne = document.getElementById('current-ability-one');
+    let currentDescriptionOne = document.getElementById('current-description-one');
+    let currentAbilityTwo = document.getElementById('current-ability-two');
+    let currentDescriptionTwo = document.getElementById('current-description-two');
     let circle = document.getElementById('circle');
 
-    newheroName.innerHTML = heroName;
-    newfirstAbility.innerHTML = firstAbility;
-    newdescriptionOne.innerHTML = descriptionOne;
-    newsecondAbility.innerHTML = secondAbility;
-    newdescriptionTwo.innerHTML = descriptionTwo;
-    
-    setUpperBox(newHeroCircle, background, heroColor);
-    createHeroCircle(circle, heroColor);
-    setTitle(newheroName, heroColor);
 
-    newHero.style.display = 'block';
+    //Text changed
+    currentName.innerHTML = name;
+    currentAbilityOne.innerHTML = abilityOne;
+    currentDescriptionOne.innerHTML = descriptionOne;
+    currentAbilityTwo.innerHTML = abilityTwo;
+    currentDescriptionTwo.innerHTML = descriptionTwo;
+
+
+    //Colors changed
+    if(color){
+        abilityBox.style.borderColor = color;
+        circle.style.backgroundColor = color;
+        currentName.style.color = color;
+        abilities[0].style.color = color;
+        abilities[1].style.color = color;
+    }
+
+    if(background){
+        abilityBox.style.background = background;
+    }
+
+    setUpperBox(heroBox, background, color);
 }
